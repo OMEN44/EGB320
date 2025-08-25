71a027ceea092031608169c1f228c834c9c0215d
OMEN44 <huonswales@gamil.com>
2025-08-25 21:14:18 +1000

    fix: removed accidentally commited build

--- software/ros_ws/build/vision/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pi/EGB320/software/ros_ws/install/vision'
--- software/ros_ws/src/vision/vision/vision_main.py
+++ software/ros_ws/src/vision/vision/vision_main.py
@@ -3,6 +3,18 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+width = 640
+height = 480
+fps = 30
+fourcc = cv2.VideoWriter_fourcc(*'YUYV') # YUYV format is often used with v4l2loopback
+output_device = '/dev/video2' # Replace with your v4l2loopback device
+
+# Create VideoWriter object
+out = cv2.VideoWriter(output_device, fourcc, fps, (width, height))
 
 class MinimalPublisher(Node):
 
@@ -12,6 +24,31 @@ class MinimalPublisher(Node):
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        while(1):
+
+            # Take each frame
+            _, frame = cap.read()
+
+            # Convert BGR to HSV
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            # define range of blue color in HSV
+            lower_blue = np.array([100,100,100])
+            upper_blue = np.array([150,150,150])
+
+            # Threshold the HSV image to get only blue colors
+            mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+            # Bitwise-AND mask and original image
+            res = cv2.bitwise_and(frame,frame, mask= mask)
+
+            # cv2.imshow('frame',frame)
+            # cv2.imshow('mask',mask)
+            out.write(mask)
+            # cv2.imshow('res',res)
+            k = cv2.waitKey(5) & 0xFF
+            if k == 27:
+                break
 
     def timer_callback(self):
         msg = String()
@@ -32,6 +69,7 @@ def main(args=None):
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
+    cv2.destroyAllWindows()
     rclpy.shutdown()
 
 
