cb3183903794be5967127c28860ba4c069194477
404AmdyNotFound <yoonbin.au@gmail.com>
2025-09-24 16:40:27 +1000

    IMU Testing, focus of Z for Gyro, yaw

--- software/python/imu_test.py
+++ software/python/imu_test.py
@@ -1,31 +1,59 @@
-import mpu6050
-import time
+__author__ = 'Majid Alekasir' # majid.alekasir@gmail.com
 
-# Create a new Mpu6050 object
-mpu6050 = mpu6050.mpu6050(0x68)
+### Some changes are applied to the original library by 'Geir Istad' ###
+### The changes aim to enhance the results of *roll-pitch-yaw* and *linear acceleration* ###
+### Some changes are made for the ease of use ###
+### You need to install 'Scipy library' which is used in 'DMP_get_roll_pitch_yaw' function. The former function was not accurate and was giving improper results ###
 
-# Define a function to read the sensor data
-def read_sensor_data():
-    # Read the accelerometer values
-    accelerometer_data = mpu6050.get_accel_data()
+"""
+MPU6050 Python I2C Class - MPU6050 example usage
+Copyright (c) 2015 Geir Istad
 
-    # Read the gyroscope values
-    gyroscope_data = mpu6050.get_gyro_data()
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    # Read temp
-    temperature = mpu6050.get_temp()
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-    return accelerometer_data, gyroscope_data, temperature
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
-# Start a while loop to continuously read the sensor data
-while True:
+from mpu6050 import MPU6050
 
-    # Read the sensor data
-    accelerometer_data, gyroscope_data, temperature = read_sensor_data()
+i2c_bus = 1
+device_address = 0x68
+freq_divider = 0x04
 
-    # Print the sensor data
-    print("Gyroscope data:", gyroscope_data['x'])
-    print("Gyroscope data:", gyroscope_data['y'])
-    print("Gyroscope data:", gyroscope_data['z'])
-    # Wait for 1 second
-    time.sleep(1)
+# Make an MPU6050
+mpu = MPU6050(i2c_bus, device_address, freq_divider)
+
+# Initiate your DMP
+mpu.dmp_initialize()
+mpu.set_DMP_enabled(True)
+
+packet_size = mpu.DMP_get_FIFO_packet_size()
+FIFO_buffer = [0]*64
+
+while True: # infinite loop
+    if mpu.isreadyFIFO(packet_size): # Check if FIFO data are ready to use...
+        
+        FIFO_buffer = mpu.get_FIFO_bytes(packet_size) # get all the DMP data here
+        
+        q = mpu.DMP_get_quaternion_int16(FIFO_buffer)
+        grav = mpu.DMP_get_gravity(q)
+        roll_pitch_yaw = mpu.DMP_get_euler_roll_pitch_yaw(q)
+        
+        print('roll: ' + str(roll_pitch_yaw.x))
+        print('pitch: ' + str(roll_pitch_yaw.y))
+        print('yaw: ' + str(roll_pitch_yaw.z))
+        print('\n')
