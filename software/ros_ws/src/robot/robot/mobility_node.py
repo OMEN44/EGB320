a4b1ac05e644137779eff975764dd7fc99429c1b
OMEN44 <huonswales@gmail.com>
2025-10-02 13:09:51 +0100

    Mobility MVP

--- software/ros_ws/src/robot/robot/mobility_node.py
+++ software/ros_ws/src/robot/robot/mobility_node.py
@@ -1,29 +1,11 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 from robot.mobility.driver import motor
 import time
 
 MAX_FORWARD_VEL = 0.5  # m/s
-WHEEL_BASE = 0.23      # meters
-
-def twist_to_pwm(twist):
-    v = twist.linear.x        # m/s
-    w = twist.angular.z       # rad/s
-
-    # wheel linear velocities (m/s)
-    v_r = v + (w * WHEEL_BASE) / 2.0
-    v_l = v - (w * WHEEL_BASE) / 2.0
-
-    # scale to [-1, 1] by dividing by MAX_FORWARD_VEL
-    pwm_r = v_r / MAX_FORWARD_VEL
-    pwm_l = v_l / MAX_FORWARD_VEL
-
-    # clip to [-1, 1]
-    pwm_r = max(-1.0, min(1.0, pwm_r))
-    pwm_l = max(-1.0, min(1.0, pwm_l))
-
-    return pwm_l, pwm_r
+WHEEL_BASE = 0.14      # meters
 
 class Mobility(Node):
     def __init__(self):
@@ -33,19 +15,21 @@ class Mobility(Node):
         # Node variables
         self.leftMotor = motor(18, 23)  # PWM pin 18, direction pin 23
         self.rightMotor = motor(14, 15) # PWM pin 14, direction pin 15
+        self.flipLeft = -1
         self.lastTime = time.time()
         self.targetPwm = [0, 0]  # [left, right]
         self.currentPwm = [0, 0] # [left, right]
-        self.dt = 0.01
+        self.dt = 0.005
         self.count = 0
         self.sign = 1
 
         # Create 100ms timer
         self.mobilityTimer = self.create_timer(self.dt, self.timer_callback)
-        self.testingTimer = self.create_timer(.4, self.test)
+        # self.testingTimer = self.create_timer(.4, self.test)
 
         # Initialise subscribers
-        self.subscription = self.create_subscription(Twist,'/cmd_vel', self.cmdCallBack, 10)
+        self.subscription = self.create_subscription(TwistStamped,'/cmd_vel', self.cmdCallBack, 10)
+        self.subscription2 = self.create_subscription(TwistStamped,'/web_vel', self.cmdCallBack, 10)
 
     def test(self):
         self.targetPwm[0] = self.count
@@ -54,12 +38,32 @@ class Mobility(Node):
         if self.count == 100 or self.count == -100:
             self.sign *= -1
 
+    
+    def twist_to_pwm(self, twist):
+        v = twist.linear.x        # m/s
+        w = twist.angular.z *2      # rad/s
+
+        # wheel linear velocities (m/s)
+        v_r = v + (w * WHEEL_BASE) / 2.0
+        v_l = v * 2 - v_r
+        # self.get_logger().info('Wheel velocities: v_l=%.2f m/s, v_r=%.2f m/s' % (v_l, v_r))
+
+        # scale to [-1, 1] by dividing by MAX_FORWARD_VEL
+        pwm_r = v_r / MAX_FORWARD_VEL
+        pwm_l = v_l / MAX_FORWARD_VEL
+
+        # clip to [-1, 1]
+        pwm_r = max(-1.0, min(1.0, pwm_r))
+        pwm_l = max(-1.0, min(1.0, pwm_l))
+
+        return pwm_l, pwm_r
+
     def timer_callback(self):
         currentTime = time.time()
 
 
         if (currentTime - self.lastTime) >= self.dt:
-            self.get_logger().info('Target PWM: left=%d, right=%d, current left=%d, right=%d' % (self.targetPwm[0], self.targetPwm[1], self.currentPwm[0], self.currentPwm[1]))
+            # self.get_logger().info('Target PWM: left=%d, right=%d, current left=%d, right=%d' % (self.targetPwm[0], self.targetPwm[1], self.currentPwm[0], self.currentPwm[1]))
             self.lastTime = currentTime
 
             # Smoothly update currentPwm towards targetPwm
@@ -70,13 +74,17 @@ class Mobility(Node):
                     self.currentPwm[i] -= 1
 
             # Update motor speeds
-            self.leftMotor.setSpeed(self.currentPwm[0])
+            self.leftMotor.setSpeed(self.flipLeft * self.currentPwm[0])
             self.rightMotor.setSpeed(self.currentPwm[1])
             self.leftMotor.motorWrite()
             self.rightMotor.motorWrite()
 
     def cmdCallBack(self, msg):
-        self.get_logger().info('Received cmd_vel message: linear_x=%f, angular_z=%f' % (msg.linear.x, msg.angular.z))
+        left_pwm, right_pwm = self.twist_to_pwm(msg.twist)
+        self.targetPwm[0] = int(left_pwm * 100)   # Scale to [-100, 100]
+        self.targetPwm[1] = int(right_pwm * 100)  # Scale to [-100, 100]
+        # self.get_logger().info('Received cmd_vel: linear.x=%.2f, angular.z=%.2f' % (msg.twist.linear.x, msg.twist.angular.z))
+        # self.get_logger().info('Converted to PWM: left=%d, right=%d' % (self.targetPwm[0], self.targetPwm[1]))
         
 
     def destroy_node(self):
