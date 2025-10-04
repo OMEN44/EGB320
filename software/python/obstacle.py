5cd1b6a188fdcd4195277b90293719400bf1d5ea
OMEN44 <huonswales@gmail.com>
2025-10-04 08:45:36 +0100

    Sensor testing

--- software/python/obstacle.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-lower_green = np.array([60, 80, 0])
-upper_green = np.array([90, 255, 255])
---
+++ software/python/sensors/tof.py
@@ -0,0 +1,9 @@
+from PiicoDev_VL53L1X import PiicoDev_VL53L1X
+from time import sleep
+
+sensor = PiicoDev_VL53L1X()
+
+while True:
+    dist = sensor.read()
+    print("Distance: {} mm".format(dist))
+    sleep(0.1)
